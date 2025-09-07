58a1d5a8dc93777c0bf38b7484376ad403b7a9e8
Ishtiaq <ishtiaq.wasi@gmail.com>
2025-09-07 19:36:44 +1000

    added categories section

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,17 @@
-const loadOnClick =()=>{
-    alert('hello')
+const loadCategories = ()=>{
+    fetch('https://openapi.programming-hero.com/api/categories')
+    .then(res=>res.json())
+    .then(data=>displayCategories(data.categories))
 }
+
+const displayCategories =(categories)=>{
+    const categoryUl = document.getElementById('category-ul');
+categories.forEach(category=>{
+   const categoryLi = document.createElement('li');
+   categoryLi.className='category-li bg-[#15803D] rounded-md text-white font-semibold p-2 mb-4';
+   categoryLi.innerText=category.category_name;
+   categoryUl.appendChild(categoryLi);
+})
+}
+
+loadCategories();
