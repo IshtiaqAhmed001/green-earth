5b405adf0335eca6449f21f13a16980780c7ed01
Ishtiaq <ishtiaq.wasi@gmail.com>
2025-09-07 19:56:46 +1000

    loaded all trees,added tree cards

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,46 @@
-const loadCategories = ()=>{
+const loadCategories = () => {
     fetch('https://openapi.programming-hero.com/api/categories')
-    .then(res=>res.json())
-    .then(data=>displayCategories(data.categories))
+        .then(res => res.json())
+        .then(data => displayCategories(data.categories))
 }
 
-const displayCategories =(categories)=>{
+const displayCategories = (categories) => {
     const categoryUl = document.getElementById('category-ul');
-categories.forEach(category=>{
-   const categoryLi = document.createElement('li');
-   categoryLi.className='category-li bg-[#15803D] rounded-md text-white font-semibold p-2 mb-4';
-   categoryLi.innerText=category.category_name;
-   categoryUl.appendChild(categoryLi);
-})
+    categories.forEach(category => {
+        const categoryLi = document.createElement('li');
+        categoryLi.className = 'category-li bg-[#15803D] rounded-md text-white font-semibold p-2 mb-4';
+        categoryLi.innerText = category.category_name;
+        categoryUl.appendChild(categoryLi);
+    })
 }
+
+constLoadAllTrees = () => {
+    fetch('https://openapi.programming-hero.com/api/plants')
+        .then(res => res.json())
+        .then(data => displayAllTrees(data.plants))
+}
+
+const displayAllTrees = (allTrees) => {
+    const treesContainer = document.getElementById('products');
+
+    allTrees.forEach(tree => {
+        const productCard = document.createElement('div');
+        productCard.className = 'product-card bg-white rounded-lg p-4';
+        productCard.innerHTML = `
+      <div class='w-full h-[150px] rounded-lg mb-2 overflow-hidden'>
+        <img class='w-full h-full' src="${tree.image}" alt="">
+      </div>
+        <h6>${tree.name}</h6>
+        <p>${tree.description}</p>
+        <button class="product-category">${tree.category}</button>
+        <h6>${tree.price}</h6>
+    `;
+        treesContainer.appendChild(productCard);
+    })
+
+
+}
+
 
 loadCategories();
+constLoadAllTrees();
