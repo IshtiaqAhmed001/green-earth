cb68ada9eb26386f99558f3fb82d82663f8f93ca
Ishtiaq <ishtiaq.wasi@gmail.com>
2025-09-08 00:00:13 +1000

    aded spinner, display category based trees

--- scripts/script.js
+++ scripts/script.js
@@ -5,35 +5,51 @@ const loadCategories = () => {
 }
 
 const displayCategories = (categories) => {
-    const categoryUl = document.getElementById('category-ul');
+    const categoryContainer = document.getElementById('category-container');
     categories.forEach(category => {
-        const categoryLi = document.createElement('li');
-        categoryLi.className = 'category-li bg-[#15803D] rounded-md text-white font-semibold p-2 mb-4';
-        categoryLi.innerText = category.category_name;
-        categoryUl.appendChild(categoryLi);
+        const categoryItem = document.createElement('div');
+        categoryItem.innerHTML =`
+        <div onClick='loadCategoryTrees(${category.id})' class='cursor-pointer hover:bg category-item bg-[#15803D] hover:bg-[#1f964b] rounded-md text-white font-semibold p-2 mb-4'>
+${category.category_name}
+        </div>
+        `
+        categoryContainer.appendChild(categoryItem);
     })
 }
 
-constLoadAllTrees = () => {
+const LoadAllTrees = () => {
+      loadSpinner(true);
     fetch('https://openapi.programming-hero.com/api/plants')
         .then(res => res.json())
-        .then(data => displayAllTrees(data.plants))
+        .then(data => displayTrees(data.plants));
 }
 
-const displayAllTrees = (allTrees) => {
+const loadCategoryTrees =(id)=>{
+    loadSpinner(true);
+fetch(`https://openapi.programming-hero.com/api/category/${id}`)
+.then(res=>res.json())
+.then(data=>displayTrees(data.plants));
+}
+
+const displayTrees = (allTrees) => {
+        loadSpinner(false)
     const treesContainer = document.getElementById('products');
+    treesContainer.innerHTML='';
 
     allTrees.forEach(tree => {
         const productCard = document.createElement('div');
-        productCard.className = 'product-card bg-white rounded-lg p-4';
+        productCard.className = 'product-card bg-white rounded-lg p-4 shadow-md h-auto';
         productCard.innerHTML = `
-      <div class='w-full h-[150px] rounded-lg mb-2 overflow-hidden'>
+      <div class='w-full h-[150px] rounded-lg overflow-hidden'>
         <img class='w-full h-full' src="${tree.image}" alt="">
       </div>
-        <h6>${tree.name}</h6>
-        <p>${tree.description}</p>
-        <button class="product-category">${tree.category}</button>
-        <h6>${tree.price}</h6>
+        <h6 class='my-2 font-bold '>${tree.name}</h6>
+        <p class='max-h-[55px] text-sm text-ellipsis overflow-hidden'>${tree.description}</p>
+        <div class='flex items-center justify-between my-4'>
+        <button class="product-category text-sm font-semibold  rounded-full py-1 px-5 bg-[#DCFCE7] text-[#15803D]">${tree.category}</button>
+        <h6 class='text-md font-semibold'>৳${tree.price}</h6>
+        </div>
+        <button class='bg-[#15803D] my-2 w-full py-1 text-white font-bold rounded-full'>Add to Cart</button>
     `;
         treesContainer.appendChild(productCard);
     })
@@ -42,5 +58,20 @@ const displayAllTrees = (allTrees) => {
 }
 
 
+const loadSpinner =(status)=>{
+const spinner = document.getElementById('spinner');
+if(status===true){
+    spinner.classList.remove('hidden')
+    console.log(spinner.classList)
+}
+if(status===false){
+    spinner.classList.add('hidden')
+        console.log(spinner.classList)
+
+}
+   
+
+}
+
 loadCategories();
-constLoadAllTrees();
+LoadAllTrees();
